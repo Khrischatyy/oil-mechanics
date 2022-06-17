fed79c7934e9c71ab215b529df4fb6a3ff66445a
khrischatyy <tony@192.168.1.5>
2022-06-17 12:42:43 +0300

    trash

--- resources/views/contents/main.blade.php
+++ resources/views/contents/main.blade.php
@@ -53,99 +53,132 @@
     left: 50%;
     right: 50%;
     margin-left: -50vw;
-    margin-right: -50vw;" id="systems" class="systems bg-green p-4">
-    <div class="text-white d-flex justify-content-center " style="margin-bottom: 30px; font-size: 45px; font-weight: 250" >
+    margin-right: -50vw;" id="systems" class="systems bg-green p-4 d-flex justify-content-center align-items-center flex-column">
+    <h4 class="text-white d-flex justify-content-center " style="margin-bottom: 30px; font-size: 45px; font-weight: 250" >
         Системы защиты погружного оборудования и кабельных линий в скважине
-    </div>
-    <div class="row d-flex justify-content-center" style="border-top: 6px solid white;">
+    </h4>
+    <div class="container-fluid row d-flex justify-content-center pt-4" style="border-top: 6px solid white;">
         @foreach($systems as $system)
-                <div class="card col-md-4 bg-light m-5 p-3" style="width: 20rem; border: none">
+                <div class="d-flex justify-content-center align-items-center card col-md-4 bg-light p-3" style="margin: 5px; border-radius: 50px; width: 25rem; border: none">
                     <img style="width: 300px" class="card-img-top" src="{{ asset($system->image) }}" alt="Card image cap">
                     <div class="card-body d-flex flex-column">
-                        <h5 class="card-title text-black">{{ $system->name }}</h5>
-                        <a style="border: 1px solid black" class="btn bg-white" href="{{route($system->childs_count > 0 ? 'childs' : 'system', ['id' => $system->id])}}">Перейти</a>
+                        <h5 class="card-title text-black text-center">{{ $system->name }}</h5>
+                        <a style="border: 1px solid black; border-radius: 15px" class="btn bg-white" href="{{route($system->childs_count > 0 ? 'childs' : 'system', ['id' => $system->id])}}">Перейти</a>
                     </div>
                 </div>
         @endforeach
     </div>
 </div>
 
-<div id="principles" class="principles mt-3">
-   <h2>
+<div id="principles" class="principles" style="margin: 40px 0 80px 0">
+   <h1 style="font-size: 45px; border-bottom: 6px solid rgba(11, 69, 0, 0.95); padding-bottom: 30px" class="text-center">
        Принципы нашей работы
-   </h2>
+   </h1>
 
-    <div class="row mt-4">
-        <div class="col-md-4">
+    <div class="row mt-5">
+        <div class="col-md-4 d-flex justify-content-center align-items-center flex-column">
                 <img class="icons justify-content-center" src="{{ asset('images/target.png') }}" alt="careful">
-            <h5 class="mt-3">Забота о клиентах</h5>
-            <p>Постоянно изучаем их потребности, предлагая новые полезные решения.
+            <h5 class="mt-3 principe-item">Забота о клиентах</h5>
+            <p class="principe-item text-center">Постоянно изучаем их потребности, предлагая новые полезные решения.
                 Мы уверены, что это — залог нашего роста и появления новых
-                долгосрочных партнерских отношений
+                долгосрочных партнерских отношений.
             </p>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-4 d-flex justify-content-center align-items-center flex-column">
             <img class="icons" src="{{ asset('images/experience.png') }}" alt="experience">
-            <h5 class="mt-3">Обмен опытом</h5>
-            <p>
+            <h5 class="mt-3 principe-item">Обмен опытом</h5>
+            <p class="principe-item text-center">
                 Мы ценим обмен идеями
                 и предложениями, поэтому наша готовность найти новые решения
-                в технологии и инновации образует ядро нашей политики ведения бизнеса
+                в технологии и инновации образует ядро нашей политики ведения бизнеса.
             </p>
         </div>
-        <div class="col-md-4">
+        <div style="position: relative; top: 20px;" class="col-md-4 d-flex justify-content-center align-items-center flex-column">
             <img class="icons" src="{{ asset('images/quality.png') }}" alt="quality">
-            <h5 class="mt-3">Качественный сервис</h5>
-            <p>
+            <h5 class="mt-3 principe-item">Качественный сервис</h5>
+            <p class="principe-item text-center">
                 Своевременная доставка, соблюдение правил охраны окружающей среды
                 и правил безопасности, но нет предела совершенству. Мы не боимся трудностей и стремиться
-                к их преодолению
+                к их преодолению.
             </p>
         </div>
     </div>
 </div>
 
-<div class="row d-flex mt-3" style="background: rgb(11, 69, 0, 0.95); width: 100vw;
-     position: relative;
-     left: 50%;
-     right: 50%;
-     margin-left: -50vw;
+        <div style="width: 100vw;
+    position: relative;
+    left: 50%;
+    right: 50%;
      color:white;
-     margin-right: -50vw;">
-    <div class="col-md-5">
-        <h2 class="mt-4">
-            География покупателей
-        </h2>
-        <p class="mt-3">
-            Наше глубокое понимание проблем в профессиональной сфере помогло нам
-            стать ведущими поставщиками технологических решений,
-            связанных с нефтяной и газовой промышленностью.
-            Компания также активно сотрудничает
-            и с представителями малых и средних
-            нефтегазодобывающих предприятий
-        </pc>
-    </div>
-    <div class="col-md-7">
-        <img class="map img-fluid" src="{{ asset('images/map.png') }}" alt="map">
-    </div>
-</div>
+    margin-left: -50vw;
+    margin-right: -50vw;" id="systems" class="systems bg-green p-4 d-flex justify-content-center align-items-center flex-column">
+            <div class="container row d-flex justify-content-center pt-4">
 
-<div class="row mt-5">
-    <h2 class="mt-4">
+                <div class="col-md-5" style="position: relative; top: -30px">
+                    <h2 class="mt-4" style="font-size:45px; border-bottom: 3px solid white; padding-bottom: 15px">
+                        География покупателей
+                    </h2>
+                    <p class="mt-3" style="font-size: 23px;">
+                        Наше глубокое понимание проблем в профессиональной сфере помогло нам
+                        стать ведущими поставщиками технологических решений,
+                        связанных с нефтяной и газовой промышленностью.
+                        Компания также активно сотрудничает
+                        и с представителями малых и средних
+                        нефтегазодобывающих предприятий.
+                    </p>
+                </div>
+                <div class="col-md-7">
+                    <img class="map" style="width: 800px" src="{{ asset('images/map.png') }}" alt="map">
+                </div>
+
+            </div>
+        </div>
+
+
+<div class="row ">
+    <h2 class="mt-4" style="padding-bottom: 20px; font-size: 45px; border-bottom: 6px solid rgba(11, 69, 0, 0.95)">
             Контакты
     </h2>
-<div class="card">
-  <div class="card-body">
-      <span class="fa fa-mobile fa-2x text-success float-right pulse" title="online now"></span>
-        <span>Телефон:</span>
-        <a href="tel:+74956779007" class="text-muted small">+7 (495) 677-90-07</a>
-        <br>
-        <span>Телефон:</span>
-        <a href="tel:+74956779008" class="text-muted small">+7 (495) 677-90-08</a>
-        <br>
-        <span>Email:</span>
-        <a href="mailto:oilmechanic@bk.ru" class="text-muted small text-truncate">oilmechanic@bk.ru</span>
-  </div>
+<div class="card" style="border: none">
+    <div class="row">
+
+        <div class="col-6 card-body" style="font-size: 20px">
+            <h3>Российская Федерация</h3>
+            <span>ОOO "Ойл Механик"</span>
+            <br>
+            <span>Телефон:</span>
+            <a href="tel:+74956779007" class="text-muted small">+7 (495) 677-90-07</a>
+            <br>
+            <span>Телефон:</span>
+            <a href="tel:+74956779008" class="text-muted small">+7 (495) 677-90-08</a>
+            <br>
+            <span>Email:</span>
+            <a href="mailto:oilmechanic@bk.ru" class="text-muted small text-truncate">oilmechanic@bk.ru</a>
+            <br>
+            <span>Адрес:</span>
+
+            <a href="mailto:oilmechanic@bk.ru" class="text-muted small text-truncate">
+                105120, г.  Москва, Нижняя Сыромятническая  улица, дом 11.
+            </a>
+        </div>
+
+        <div class="col-6 card-body" style="font-size: 20px">
+            <h3>Республика Казахстан</h3>
+            <span>TOO "Oil Mechanic Caspian"</span>
+            <br>
+            <span>Телефон:</span>
+            <a href="tel:+7 778 308 73 67" class="text-muted small">+7 778 308 73 67</a>
+            <br>
+            <span>Email:</span>†
+            <a href="mailto:om.caspian@mbx.kz" class="text-muted small text-truncate">om.caspian@mbx.kz</a>
+            <br>
+            <span>Адрес:</span>
+
+            <a href="mailto:oilmechanic@bk.ru" class="text-muted small text-truncate">
+                130000, г. Актау БЦ «Forum» 14мкр. строение 58/1
+            </a>
+        </div>
+    </div>
 </div>
 </div>
 @endsection
